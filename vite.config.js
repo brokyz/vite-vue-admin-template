d87b20a5683cc580eb44bc40c2f590caa643f056
boqiang <boqiang6450@163.com>
2024-03-01 11:15:17 +0800

    添加vite代理服务器

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
-import vue from "@vitejs/plugin-vue2";
-import { resolve } from "path";
+import vue from '@vitejs/plugin-vue2'
+import { resolve } from 'path'
 
 export default {
   plugins: [vue()],
-  base: "./",
+  base: './',
   resolve: {
     // 设置@为src目录
     alias: {
-      "@": resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
     // 设置允许自动导入的后缀名文件
-    extensions: [".mjs", ".js", ".mts", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
-};
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://mock.apifox.com/m1/4086383-0-default',
+        changeOrigin: true,
+      },
+    },
+  },
+}
